314afeb7990a5ff0355cbec68e68a9547be7ade3
Lara <laraayumi56@gmail.com>
2021-07-29 22:50:17 -0300

    python testes roda isso ai

--- EP2/run.py
+++ EP2/run.py
@@ -6,7 +6,9 @@ def main():
     path_csv = input("Digite o nome do csv: ")
     repeticoes = input("Digite o numero de repeticoes: ")
     n_proc = [2, 4, 8, 16]
+    #n_proc = [2, 4]
     n_thread = [1, 2, 4, 8, 16, 32]
+    #n_thread = [4, 8]
     lists = []
     dif_thread = []
     dif_proc = []
@@ -23,16 +25,18 @@ def main():
                 result = subprocess.run(output, stdout=PIPE)
                 # Transforma seq. de bytes em lista de string
                 str_b = (result.stdout).decode('ascii')
+                #print(str_b)
                 #inputs = list(map(str, str_b.split()))
                 lists.append(str_b)
 
-            dif_thread.append(lists)
-        dif_proc.append(dif_thread)
+            #dif_thread.append(lists)
+        #dif_proc.append(dif_thread)
 
     #print(dif_proc)
 
-    # Escreve resultado no csv...
+    # Escreve resultado no csv... 
     rows = []
+    x = 0
     fields = ['execution', 'n_threads', 'n_process', 'time']
     for i in range(len(n_proc)):
         for j in range(len(n_thread)):
@@ -41,9 +45,12 @@ def main():
                 row.append(str(q+1))
                 row.append(str(n_thread[j]))
                 row.append(str(n_proc[i]))
-                row.append(str(dif_proc[i][j][q]))
+                #row.append(lists[x])
+                row.append(str(lists[x]))
+                x += 1
                 #print(row)
-                rows.append(row)
+                rows.append(row[:])
+                #print(rows)
 
     with open(path_csv, 'w', newline='') as file: 
         writer = csv.writer(file)
